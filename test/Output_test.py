c35cfc4db49e3ec04c9841c986869bc90d321627
Sam Crawford <sam.crawford82@gmail.com>
2020-09-11 13:27:19 -0400

    Added Output_test; part of #4

---
+++ test/Output_test.py
@@ -0,0 +1,56 @@
+from os import curdir, path, remove
+import sys
+
+sys.path.append(path.abspath("src"))
+
+from Output import saveToFile
+
+
+class TestSaveToFile:
+    outDir = path.join(curdir, "test", "output")
+    stableDir = path.join(curdir, "test", "stable")
+
+    def test_file_contents_correct(self):
+        data = ['<?xml version="1.0" encoding="UTF-16"?>\n',
+                '<POLL TYPE="named" SHOWTIMER="yes" ALARM="1:45"' +
+                ' NOANSWER="yes" SHOWRESPONSE="yes">\n',
+                '\n', '<QUESTION TYPE="mcmany"' +
+                ' TITLE="Will&nbsp;this&nbsp;code&nbsp;work?">\n',
+                '<ANSWER ISCORRECT="true">Yes</ANSWER>\n',
+                '<ANSWER ISCORRECT="false">No</ANSWER>\n',
+                '<ANSWER ISCORRECT="true">Hopefully</ANSWER>\n',
+                '<ANSWER ISCORRECT="false">Who&nbsp;knows?</ANSWER>\n',
+                '</QUESTION>\n', '\n', '</POLL>']
+
+        saveToFile(path.join(self.outDir, "oneMCManyOut-"), data)
+
+        fileName = "oneMCManyOut-1.atp"
+        outPath = path.join(self.outDir, fileName)
+
+        assert path.isfile(outPath)
+
+        with open(path.join(outPath)) as f:
+            out = f.readlines()
+        with open(path.join(self.stableDir, fileName)) as f:
+            stableOut = f.readlines()
+
+        assert out == stableOut
+
+        # clean up
+        remove(path.join(outPath))
+
+    def test_numbering(self):
+        data = ["\n"]
+        pathPreface = path.join(self.outDir, "fileExists-")
+
+        saveToFile(pathPreface, data)
+        saveToFile(pathPreface, data)
+        saveToFile(pathPreface, data)
+
+        assert path.isfile(pathPreface + "1.atp")
+        assert path.isfile(pathPreface + "4.atp")
+        assert path.isfile(pathPreface + "6.atp")
+
+        remove(pathPreface + "1.atp")
+        remove(pathPreface + "4.atp")
+        remove(pathPreface + "6.atp")
